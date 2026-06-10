5404dbab7a7c86150422481503da6aded995acb2
Arpita-Panda-1 <technocraft1969@gmail.com>
2026-06-10 23:53:02 +0530

    DB connection correction

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+    console.log(process.env.MONGO_URI);
     try {
         await mongoose.connect(
             process.env.MONGO_URI
         );
-
         console.log(
             "✅ MongoDB Connected Successfully"
         );
